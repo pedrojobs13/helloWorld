d88abf72eaeb3ba68c0951c4d71e7bcf9dd6d948
Pedro Lucas Gomes <pedrolucasgomesdeoliveira13@gmail.com>
2025-04-21 21:47:20 -0300

    first commit

---
+++ src/main/java/com/example/helloworld/controller/Hello.java
@@ -0,0 +1,15 @@
+package com.example.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hello")
+public class Hello {
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+}
